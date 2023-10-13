df6e6834d836cec4a5a542b9a239f2abf4cbe78a
sheraz-ayman <sheraz.ayman@gmail.com>
2023-10-13 16:58:51 +0300

    add todo

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import axios from 'axios'
 
 function Create() {
+  const [task, setTask] = useState()
+
+  const handleAdd = () =>{
+    axios.post('http://192.168.100.8:5000/add' , {task:task})
+    .then(result => console.log(result))
+    .catch(err=> console.log(err))
+  }
   return (
     <div>
-        <input type='text' name='' id=""/>
-        <button type="button">Add</button>
+        <input type='text' onChange={(e)=> setTask(e.target.value)}/>
+        <button type="button" onClick={handleAdd}>Add</button>
     </div>
   )
 }
