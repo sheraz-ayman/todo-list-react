f0d0ac608b831af37fca53bfd7681d971bd1da1c
sheraz-ayman <sheraz.ayman@gmail.com>
2023-10-13 19:16:07 +0300

    design

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Create from '../components/Create';
+import './Home.css';
 import axios from 'axios';
-import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill } from 'react-icons/bs'; // Import icons from react-icons
+import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill } from 'react-icons/bs';
 
 function Home() {
   const [todos, setTodos] = useState([]);
@@ -15,39 +16,39 @@ function Home() {
 
   const handleEdit = (id) => {
     axios
-      .put(`http://192.168.100.8:5000/update/${id}`) // Include a slash before id
+      .put(`http://192.168.100.8:5000/update/${id}`)
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
-  };
-  
+  }
+
   const handleDelete = (id) => {
     axios
-      .delete(`http://192.168.100.8:5000/delete/${id}`) // Include a slash before id
-      .then((result) => console.log(result))     
-       .catch((err) => console.log(err));
-  };
-  
+      .delete(`http://192.168.100.8:5000/delete/${id}`)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  }
 
   return (
-    <div>
+    <div className="container">
       <h2>Todo list</h2>
       <Create />
       {todos.length === 0 ? (
-        <div>
+        <div className="no-records">
           <h2>No records</h2>
         </div>
       ) : (
         todos.map((todo) => (
-          <div > 
-          <div className="checkbox" onClick={()=>handleEdit(todo._id)}>
-            {todo.done? 
-            <BsFillCheckCircleFill  className="icon"/>
-            : <BsCircleFill className="icon" />
-        }
-       <p >{todo.task}</p> 
-          </div>
-         
-            <BsFillTrashFill className="icon" onClick={()=> handleDelete(todo._id)}/>
+          <div key={todo._id} className="checkbox">
+            <div onClick={() => handleEdit(todo._id)}>
+              {todo.done ? (
+                <BsFillCheckCircleFill className="icon checked" />
+              ) : (
+                <BsCircleFill className="icon" />
+              )
+              }
+              <p>{todo.task}</p>
+            </div>
+            <BsFillTrashFill className="icon" onClick={() => handleDelete(todo._id)} />
           </div>
         ))
       )}
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './Create.css';
 
-function Create({ onAddTask }) {
-  const [task, setTask] = useState();
+function Create() {
+  const [task, setTask] = useState('');
 
   const handleAdd = () => {
-    axios.post('http://192.168.100.8:5000/add', { task: task })
-      .then(result => {
-        onAddTask(); // Call the callback function to reload the page
+    axios
+      .post('http://192.168.100.8:5000/add', { task: task })
+      .then((result) => {
+        console.log(result.data);
       })
-      .catch(err => console.log(err));
-  }
+      .catch((err) => console.log(err));
+  };
 
   return (
-    <div>
-      <input type='text' onChange={(e) => setTask(e.target.value)} />
-      <button type="button" onClick={handleAdd}>Add</button>
+    <div className="create-container">
+      <input
+        type="text"
+        className="input-field"
+        placeholder="Enter your task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type="button" className="add-button" onClick={handleAdd}>
+        Add
+      </button>
     </div>
   );
 }
